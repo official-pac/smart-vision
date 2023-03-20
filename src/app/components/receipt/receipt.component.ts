d18f36f8088cd4c57ab3cab2baf8e9ec2a37df8d
Arun Chandran <arun.chandran@rehau.com>
2023-03-20 15:54:59 +0530

    added functionality to redirect from receipt to search page after 5 seconds

--- src/app/components/receipt/receipt.component.ts
+++ src/app/components/receipt/receipt.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { timer, take, map, interval, takeUntil, takeWhile } from 'rxjs';
 import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -15,17 +17,25 @@ export class ReceiptComponent implements OnInit {
   carRegistrationNumber!: string;
   transactionTime!: number;
   totalAmount!: number;
-  constructor(private storageService: StorageService) { }
+  secondsLeft!: number;
+  constructor(private storageService: StorageService, private router: Router) { }
 
   ngOnInit(): void {
     this.initDetails();
+    this.showTimer();
   }
 
   private initDetails() {
     this.carRegistrationNumber = this.storageService.userDetails?.rcNumber;
     this.totalAmount = this.storageService.slotDetails?.charge;
-    this.transactionId = `MH${Math.floor(Math.random() * 999)}`; // 5 digit with 2 alphabet
+    this.transactionId = `MH${Math.floor(Math.random() * 999)}`;
     this.transactionTime = this.storageService.transactionTime;
+  }
+
+  showTimer() {
+    const source = timer(0, 1000);
+    source.pipe(takeWhile(val => val <= 6))
+      .subscribe(val => val < 6 ? this.secondsLeft = 5 - val : this.router.navigate(['/']));
   }
 
 }
