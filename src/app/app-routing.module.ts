df7ba868453ac260da4d40b3f0ae7d403b4cf43b
Arun Chandran <arun.chandran@rehau.com>
2023-03-21 12:37:30 +0530

    type: development comment: added new authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 // TODO: Make components standalone of feasible
 // TODO: Set title as Parking Automation - Search
@@ -17,20 +18,24 @@ const routes: Routes = [
   {
     path: 'car-details',
     loadComponent: () => import('./components/car-details/car-details.component')
-      .then((mod) => mod.CarDetailsComponent)
+      .then((mod) => mod.CarDetailsComponent),
+    canActivate: [AuthGuard]
   },
   {
     path: 'slots',
     loadComponent: () => import('./components/parking-slot-selection/parking-slot-selection.component')
-      .then((mod) => mod.ParkingSlotSelectionComponent)
+      .then((mod) => mod.ParkingSlotSelectionComponent),
+    canActivate: [AuthGuard]
   },
   {
     path: 'payment',
-    loadComponent: () => import('./components/payments/payments.component').then((mod) => mod.PaymentsComponent)
+    loadComponent: () => import('./components/payments/payments.component').then((mod) => mod.PaymentsComponent),
+    canActivate: [AuthGuard]
   },
   {
     path: 'receipt',
-    loadComponent: () => import('./components/receipt/receipt.component').then((mod) => mod.ReceiptComponent)
+    loadComponent: () => import('./components/receipt/receipt.component').then((mod) => mod.ReceiptComponent),
+    canActivate: [AuthGuard]
   },
   { path: '', pathMatch: 'full', redirectTo: 'search' },
   { path: '**', redirectTo: 'search' }
